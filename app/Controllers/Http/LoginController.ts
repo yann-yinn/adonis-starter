b5c52a8c65a80a0ba45aeb1642425e8d6b73cf3b
yann <yann.boisselier@gmail.com>
2021-09-13 11:24:52 +0200

    log login

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -12,12 +12,17 @@ export default class LoginController {
    * Handle login form submissions
    */
   public async store({ request, auth, response }: HttpContextContract) {
+    console.log(request.input("email"), request.input("password"));
     /**
      * Attempt to login the user with the email and password. The
      * "auth.attempt" method will perform all the required
      * validations.
      */
-    await auth.attempt(request.input("email"), request.input("password"));
+    try {
+      await auth.attempt(request.input("email"), request.input("password"));
+    } catch (error) {
+      console.log(error);
+    }
 
     /**
      * Redirect to the home page
