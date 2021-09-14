7dc343c0c60899700d1e5151cd6e47d6cd54f8ed
yann <yann.boisselier@gmail.com>
2021-09-14 10:07:37 +0200

    register silentAuth as a global middleware

--- start/kernel.ts
+++ start/kernel.ts
@@ -23,6 +23,7 @@ import Server from "@ioc:Adonis/Core/Server";
 Server.middleware.register([
   () => import("@ioc:Adonis/Core/BodyParser"),
   () => import("@ioc:Adonis/Addons/Shield"),
+  () => import("App/Middleware/SilentAuth"),
 ]);
 
 /*
@@ -43,4 +44,5 @@ Server.middleware.register([
 */
 Server.middleware.registerNamed({
   auth: () => import("App/Middleware/Auth"),
+  silentAuth: () => import("App/Middleware/SilentAuth"),
 });
