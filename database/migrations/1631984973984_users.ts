0b9399a433401fa635a557530d24c58c1586b74d
yann <yann.boisselier@gmail.com>
2021-09-18 19:12:49 +0200

    add roles column

---
+++ database/migrations/1631984973984_users.ts
@@ -0,0 +1,11 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class Users extends BaseSchema {
+  protected tableName = "users";
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.specificType("roles", "VARCHAR(255)[]");
+    });
+  }
+}
