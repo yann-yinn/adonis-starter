99f33d581db5dd02fa28eed75f4d7ccd85c1dfc8
yann <yann.boisselier@gmail.com>
2021-10-01 15:12:16 +0200

    move root code routines to User model

--- app/Services/UserService.ts
+++ app/Services/UserService.ts
@@ -55,15 +55,11 @@ async function create(payload: createUserPayload, options?: createOptions) {
   user.emailVerified = false;
   user.blocked = config.signup.blockUserUntilEmailVerification ? true : false;
 
-  // special case: first created user is automatically the super admin
-  if ((await User.first()) === null) {
-    user.roles = ["root"];
+  if (payload.role) {
+    user.roles = [payload.role];
   } else {
-    if (payload.role) {
-      user.roles = [payload.role];
-    } else {
-      user.roles = ["member"];
-    }
+    // defaut role is member, if no role has been provided.
+    user.roles = ["member"];
   }
 
   user.password = payload.password.trim();
