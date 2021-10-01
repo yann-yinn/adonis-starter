d0fd56743e8df95ba3bd5a49376f1e2e1353625d
Sreejit7 <sreejitde7@gmail.com>
2021-10-01 08:34:18 +0530

    Added password show/hide toggle to all required forms

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import '../css/app.css'
+const pwToggleBtn = document.getElementById('togglePassword');
+const pwToggleConfirmBtn = document.getElementById('togglePasswordConfirm');
+const pwInput = document.getElementById('password');
+const pwConfirmInput = document.getElementById('password-confirm');
+
+const togglePassword = function(){
+  const type = pwInput.getAttribute("type") === "password"? "text": "password";
+  pwInput.setAttribute("type", type);
+  this.classList.toggle('bi-eye');
+}
+
+const togglePasswordConfirm = function(){
+  const type = pwConfirmInput.getAttribute("type") === "password"? "text": "password";
+  pwConfirmInput.setAttribute("type", type);
+  this.classList.toggle('bi-eye');
+}
+pwToggleBtn.addEventListener("click", togglePassword);
+pwToggleConfirmBtn.addEventListener("click", togglePasswordConfirm);
