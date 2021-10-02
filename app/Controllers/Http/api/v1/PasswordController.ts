576b492224abaa5430d33a6954c33b0f63be6daf
yann <yann.boisselier@gmail.com>
2021-10-02 12:09:05 +0200

    password strength api: create one error by rule automatically from password rules

--- app/Controllers/Http/api/v1/PasswordController.ts
+++ app/Controllers/Http/api/v1/PasswordController.ts
@@ -3,21 +3,27 @@ import { validator, schema } from "@ioc:Adonis/Core/Validator";
 import { passwordRules } from "App/Validators/UserValidators";
 
 export default class PasswordController {
-  public async strength({ params }: HttpContextContract) {
+  public async strength({ params }: HttpContextContract): Promise<{
+    success: boolean;
+    errors: object | null;
+  }> {
     const password = decodeURIComponent(params.password);
+    const errorsSchema = {};
+    const data = {};
+    // create errors from existing passwordRules
+    passwordRules(false).forEach((rule) => {
+      errorsSchema[rule.name] = schema.string({ trim: true }, [rule]);
+      data[rule.name] = password;
+    });
     try {
       await validator.validate({
-        schema: schema.create({
-          password: schema.string({ trim: true }, passwordRules(false)),
-        }),
-        data: {
-          password,
-        },
+        schema: schema.create(errorsSchema),
+        data,
       });
     } catch (error) {
-      return { success: false, errors: error.messages.password };
+      return { success: false, errors: error.messages };
     }
-    return { success: true, errors: [] };
+    return { success: true, errors: null };
   }
   public async test({ view }: HttpContextContract) {
     return view.render("pages/passwordTest");
