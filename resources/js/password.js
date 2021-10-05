49bbc05df767048d1504814b84bb84fbddb6bba4
yann <yann.boisselier@gmail.com>
2021-10-05 11:54:15 +0200

    show password: querySelector on name instead of id

--- resources/js/password.js
+++
@@ -1,8 +0,0 @@
-const pwToggleBtn = document.getElementById('togglePassword');
-const pwInput = document.getElementById('password');
-
-pwToggleBtn.addEventListener("click", function(){
-  const type = pwInput.getAttribute("type") === "password"? "text": "password";
-  pwInput.setAttribute("type", type);
-  this.classList.toggle('bi-eye');
-});
